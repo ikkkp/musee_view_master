aebfac6ed7613aa2506ae145fff903010c573ec0
ikkkp <2496450092@qq.com>
2024-01-23 21:53:11 +0800

    Initial commit

---
+++ src/components/TopNavComponent.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="topbox">
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/CaptureMainView.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>ss</div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+</style>
