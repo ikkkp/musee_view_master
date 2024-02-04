bfae12469afcd05940f4c9ef498f3417695f43f2
ikkkp <2496450092@qq.com>
2024-02-04 13:59:29 +0800

    new file:   public/worklet/vumeter.js 	modified:   src/components/ConversationComponent/ConversationComponents.vue 	new file:   src/hooks/useVolume/useVolume.js

--- src/components/ConversationComponent/ConversationComponents.vue
+++ src/components/ConversationComponent/ConversationComponents.vue
@@ -1,16 +1,11 @@
 <template>
     <v-expand-transition class="conversation-container">
         <v-card v-if="overlayValue" class="v-card--reveal" style="height: 100%;">
-            <v-card-text class="pb-0">
-                <p class="text-h4 text--primary">
-                    Origin
-                </p>
-                <p>late 16th century (as a noun denoting a place where alms were distributed): from medieval Latin
-                    eleemosynarius, from late Latin eleemosyna ‘alms’, from Greek eleēmosunē ‘compassion’ </p>
-            </v-card-text>
-            <GPTSVGComponent />
+            <GPTSVGComponent class="svg-display" :style="{ 'width': computedWidth }" />
+            {{ volume }}
             <v-card-actions>
-                <v-btn :class="['compact-button', 'icon-button']" icon="mdi-arrow-up-circle" @click="toggleOverlay" color="#2081C3">
+                <v-btn :class="['compact-button', 'icon-button']" icon="mdi-arrow-up-circle" @click="toggleOverlay"
+                    color="#2081C3">
                     <svg-icon type="mdi" :path="mdiMicrophoneOff" class="expand-icon"></svg-icon>
                 </v-btn>
             </v-card-actions>
@@ -19,33 +14,46 @@
 </template>
 
 <script setup>
-import { defineProps, ref, watch } from 'vue';
+import { defineProps, ref, watch, onMounted } from 'vue';
 import SvgIcon from '@jamescoyle/vue-icon';
 import GPTSVGComponent from '@/components/GPTSVGComponent.vue';
 import { mdiMicrophoneOff } from '@mdi/js'
-
-
+import useVolume from "@/hooks/useVolume/useVolume";
+const volume = ref(0)
 const props = defineProps({
     overlay: {
         type: Boolean,
         default: false,
     },
 });
-
-
+const emit = defineEmits(['update:overlay']);
 const overlayValue = ref(props.overlay);
-
+const computedwidth = ref(0);
+onMounted(() => {
+    navigator.mediaDevices
+        .getUserMedia({ video: false, audio: true })
+        .then((s) => {
+            volume.value = useVolume(s);
+        });
+})
+function computedWidth() {
+    // 根据 volume 值映射到宽度范围
+    // 这里假设音量最大为 100，对应的最大宽度为 300px
+    return `${100 + volume * 20}px`;
+}
 watch(() => props.overlay, (val) => {
     overlayValue.value = val;
 });
 
-
-const emit = defineEmits(['update:overlay']);
+watch(() => volume, (val) => {
+    computedwidth.value = 100+val*20;
+});
 
 function toggleOverlay() {
     overlayValue.value = !overlayValue.value;
     emit('update:overlay', overlayValue.value);
     console.log(overlayValue.value);
+    stopRecording();
 }
 </script>
 
@@ -55,22 +63,31 @@ function toggleOverlay() {
     display: flex;
     flex-direction: column;
     align-items: center;
-}
+    justify-content: flex-end;
 
-.compact-button {
-    density: compact;
-    elevation: 0;
-    width: 30px;
-    height: 30px;
-}
-
-.icon-button {
-    width: 50px;
-    height: 50px;
 }
 
 .expand-icon {
     height: 50px;
     width: 50px;
+}
+
+@keyframes rotate {
+    from {
+        transform: rotate(0deg);
+    }
+
+    to {
+        transform: rotate(-360deg);
+    }
+}
+
+.svg-display {
+
+    animation: rotate 5s linear infinite;
+    /* 使用rotate动画，设置旋转时间、动画速度和无限循环 */
+    transform-origin: center;
+    /* 设置旋转的中心点为元素中心 */
+    margin: 10px 20px 80px 20px;
 }
 </style>
---
+++ src/hooks/useVolume/useVolume.js
@@ -0,0 +1,60 @@
+import {reactive,ref,watch} from 'vue'
+export default function useVolume(stream){
+    const volume = ref(0)
+    const newRef = reactive({current:{}})
+    const onmessage = (event) => {
+        if (!newRef.current.audioContext) {
+            return;
+        }
+        if (event.data.volume) {
+            volume.value = Math.round(event.data.volume * 200)
+        }
+    };
+    const disconnectAudioContext = () =>{
+        if (newRef.current.node) {
+            try {
+                newRef.current.node.disconnect();
+            } catch (errMsg) {}
+        }
+        if (newRef.current.source) {
+            try {
+                newRef.current.source.disconnect();
+            } catch (errMsg) {}
+        }
+        newRef.current.node = null;
+        newRef.current.source = null;
+        newRef.current.audioContext = null;
+        volume.value = 0
+    }
+    const connectAudioContext = async (mediaStream) =>{
+        if (newRef.current.audioContext) {
+                disconnectAudioContext();
+            }
+            try {
+                newRef.current.audioContext = new AudioContext();
+                await newRef.current.audioContext.audioWorklet.addModule('./worklet/vumeter.js');
+                if (!newRef.current.audioContext) {
+                    return;
+                }
+                newRef.current.source = newRef.current.audioContext.createMediaStreamSource(mediaStream);
+                newRef.current.node = new AudioWorkletNode(newRef.current.audioContext, 'vumeter');
+                newRef.current.node.port.onmessage = onmessage;
+                newRef.current.source.connect(newRef.current.node).connect(newRef.current.audioContext.destination);
+            } catch (errMsg) {
+                disconnectAudioContext();
+            }
+    }
+    watch(()=>stream,(nVal,oVal)=>{
+        if (!nVal) {
+            return () => {};
+        }
+        connectAudioContext(nVal);
+        return () => {
+            disconnectAudioContext(nVal);
+        };
+    },{
+        immediate:true
+    })
+
+    return volume
+}
