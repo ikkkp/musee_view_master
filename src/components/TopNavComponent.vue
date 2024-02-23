e70d4d7c281570e0c0fd8cb0d8accb03f303265e
ikkkp <2496450092@qq.com>
2024-02-23 18:18:39 +0800

    modified:   src/components/TopNavComponent.vue

--- src/components/TopNavComponent.vue
+++ src/components/TopNavComponent.vue
@@ -19,7 +19,7 @@
             <v-btn rounded="xl" size="x-large" variant="outlined" color="#388FCA"
                 @click="() => ConversationShow = true">About
                 us</v-btn>
-            <v-avatar color="info" size="45px">
+            <v-avatar color="info" size="45px" @click="()=> dialog=true">
                 <span class="text-h6">IK</span>
             </v-avatar>
         </div>
@@ -32,7 +32,8 @@
                     <v-container>
                         <v-row>
                             <v-col cols="12" sm="6">
-                                <v-img src="src/images/musee-edu/musee-edu.webp" aspect-ratio="1.7" class="mx-auto" cover :height="300"></v-img>
+                                <v-img src="src/images/musee-edu/musee-edu.webp" aspect-ratio="1.7" class="mx-auto" cover
+                                    :height="300"></v-img>
                             </v-col>
                             <v-col cols="12" sm="6">
                                 {{ intro }}
@@ -46,7 +47,59 @@
                 </v-card-actions>
             </v-card>
         </v-dialog>
-
+        <template>
+            <v-row justify="center">
+                <v-dialog v-model="dialog" persistent width="1024">
+                    <v-card>
+                        <v-card-title>
+                            <span class="text-h5">个人信息</span>
+                        </v-card-title>
+                        <v-card-text>
+                            <v-container>
+                                <v-row>
+                                    <v-col cols="12" sm="6" md="4">
+                                        <v-text-field label="昵称" required></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" sm="6" md="4">
+                                        <v-text-field label="账号"
+                                            hint="example of helper text only on focus"></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" sm="6" md="4">
+                                        <v-text-field label="学历" hint="example of persistent helper text"
+                                            persistent-hint required></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12">
+                                        <v-text-field label="邮箱*" required></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12">
+                                        <v-text-field label="密码*" type="password" required></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" sm="6">
+                                        <v-select :items="['0-6', '7-12', '12-15', '18+']" label="Age*"
+                                            required></v-select>
+                                    </v-col>
+                                    <v-col cols="12" sm="6">
+                                        <v-autocomplete
+                                            :items="['语文', '数学', '英语', '物理', '化学', '生物', '历史', '政治', '地理']"
+                                            label="专业" multiple></v-autocomplete>
+                                    </v-col>
+                                </v-row>
+                            </v-container>
+                            <small>*项必填</small>
+                        </v-card-text>
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn color="blue-darken-1" variant="text" @click="dialog = false">
+                                Close
+                            </v-btn>
+                            <v-btn color="blue-darken-1" variant="text" @click="dialog = false">
+                                Save
+                            </v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-dialog>
+            </v-row>
+        </template>
     </div>
 </template>
 
@@ -54,6 +107,7 @@
 import { onMounted, ref } from 'vue';
 import router from '@/router';
 const ConversationShow = ref(false);
+const dialog= ref(false);
 const intro = ref("沐斯慧教（MuseeEducation）致力于运用人工智能大模型推动教育的智慧化转型。通过结合先进的AI技术和教育行业的深刻理解，能够为学习者提供更加个性化、互动性强和效率高的学习体验。沐斯慧教的核心理念是利用大数据分析、自然语言处理和机器学习等技术，为教育领域带来创新的解决方案，包括但不限于个性化学习路径推荐、智能辅导机器人、以及基于学习者表现的实时反馈系统。");
 const buttons = ref([
     {
