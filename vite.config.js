a2d643f23eca332986d93051ad8bb781045c454c
ikkkp <2496450092@qq.com>
2024-04-09 23:57:19 +0800

    renamed:    dist/assets/ChatGPT-BEA3euge.png -> dist/assets/ChatGPT-RAN3roHj.png 	renamed:    dist/assets/index-Dn-vmSo8.js -> dist/assets/index-PXD6tU4j.js 	deleted:    dist/assets/index-pm1aFo7R.css 	new file:   dist/assets/index-s5Lh5X9n.css 	renamed:    dist/assets/materialdesignicons-webfont-Dp5v-WZN.woff2 -> dist/assets/materialdesignicons-webfont-6eb_lmTU.woff2 	renamed:    dist/assets/materialdesignicons-webfont-PXm3-2wK.woff -> dist/assets/materialdesignicons-webfont-D15t_tsC.woff 	renamed:    dist/assets/materialdesignicons-webfont-B7mPwVP_.ttf -> dist/assets/materialdesignicons-webfont-e5j8FT_3.ttf 	renamed:    dist/assets/materialdesignicons-webfont-CSr8KVlo.eot -> dist/assets/materialdesignicons-webfont-kq_ClZaA.eot 	renamed:    dist/assets/no_data-DveCDKkH.svg -> dist/assets/no_data-73ggypBy.svg 	modified:   dist/index.html 	modified:   package-lock.json 	modified:   package.json 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-
+import vueImages from 'vite-plugin-vue-images';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   plugins: [
     vue(),
     vueJsx(),
+    vueImages(),
   ],
   resolve: {
     alias: {
